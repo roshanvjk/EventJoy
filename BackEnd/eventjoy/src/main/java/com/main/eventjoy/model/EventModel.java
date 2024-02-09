6b85e42b5218f136f481446600067e6a4df17d24
beast2727 <72391756+beast2727@users.noreply.github.com>
2024-02-09 09:52:53 +0530

    Backend Completed

---
+++ BackEnd/eventjoy/src/main/java/com/main/eventjoy/model/EventModel.java
@@ -0,0 +1,41 @@
+package com.main.eventjoy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class EventModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int eventId;
+    private String eventName;
+    private String applicantAddress;
+    private int attendees;
+    private String applicantMobile;
+    private String reference;
+    private String eventAddress;
+    private String eventDescription;
+    private String eventDate;
+    private String eventTime;
+    private int eventThemeId;
+    private String eventFoodId;
+    private int addonId;
+    private String eventCost;
+    @ManyToOne
+    @JsonIgnore
+    private UserModel userModel;
+}
---
+++ BackEnd/eventjoy/src/main/java/com/main/eventjoy/model/FoodModel.java
@@ -0,0 +1,31 @@
+package com.main.eventjoy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FoodModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int foodId;
+    private String foodName;
+    private String foodImageURL;
+    private String foodDescription;
+    private String foodPrice;
+    @ManyToOne
+    @JsonIgnore
+    private AdminModel adminModel; 
+}
